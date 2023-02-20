7b94f727d70c2893c0f07e0fa7db2b512281880d
Maksym <111660402+sunmadeq@users.noreply.github.com>
2023-02-20 23:02:51 +0100

    Removed hard code

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
 const items = document.querySelectorAll('.nav__item');
-const indicator = document.querySelector('.nav__indicator');
 let activeItem = document.querySelector('.nav__item[active]');
 
-for (let i in items) {
-  let item = items[i];
+for (let item of items) {
   item.addEventListener('click', () => {
-    if (item !== activeItem) {
-      activeItem.removeAttribute('active');
-      item.setAttribute('active', '');
-      activeItem = item;
-      indicator.style.left = i * 96 + 'px';
-    }
+    if (item === activeItem) return;
+
+    activeItem.removeAttribute('active');
+    item.setAttribute('active', '');
+    activeItem = item;
   });
 }
